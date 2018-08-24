66cada50d51c3b682a8be212c76bb21e77c987a4
Mikael Skeneby <mikael.skeneby@outlook.com>
2018-08-24 06:46:13 +0200

    Wokring on new version

--- Droid/Bootstrap/ConnectivityPluginBootstrap.cs
+++
@@ -1,7 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace stops.Bootstrap
-{
-    public class ConnectivityPluginBootstrap
-        : MvxPluginBootstrapAction<Cheesebaron.MvxPlugins.Connectivity.PluginLoader> { }
-}
